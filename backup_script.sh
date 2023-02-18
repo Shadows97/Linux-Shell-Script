78c871ac4c663a16e6d3051ca6e4fc1b973a4888
M'PO Derrick Charbel <40666118+Shadows97@users.noreply.github.com>
2023-02-18 17:13:15 +0000

    update backup script

--- backup_script.sh
+++ backup_script.sh
@@ -18,10 +18,18 @@ DATE=$(date +"%Y-%m-%d")
 
 # Backup PostgreSQL
 POSTGRES_BACKUP_FILENAME="${POSTGRES_DB_NAME}_${DATE}.sql"
+POSTGRES_BACKUP_DIR="${BACKUP_DIR}dingastream/"
 docker exec ${POSTGRES_CONTAINER_NAME} pg_dump -U ${POSTGRES_USER} -d ${POSTGRES_DB_NAME} -f /backup/${POSTGRES_BACKUP_FILENAME}
-docker cp ${POSTGRES_CONTAINER_NAME}:/backup/${POSTGRES_BACKUP_FILENAME} ${BACKUP_DIR}
+docker cp ${POSTGRES_CONTAINER_NAME}:/backup/${POSTGRES_BACKUP_FILENAME} ${POSTGRES_BACKUP_DIR}
 
 # Backup MySQL
 MYSQL_BACKUP_FILENAME="${MYSQL_DB_NAME}_${DATE}.sql"
+MYSQL_BACKUP_DIR="${BACKUP_DIR}artists/"
 docker exec ${MYSQL_CONTAINER_NAME} mysqldump -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DB_NAME} > /backup/${MYSQL_BACKUP_FILENAME}
-docker cp ${MYSQL_CONTAINER_NAME}:/backup/${MYSQL_BACKUP_FILENAME} ${BACKUP_DIR}
+docker cp ${MYSQL_CONTAINER_NAME}:/backup/${MYSQL_BACKUP_FILENAME} ${MYSQL_BACKUP_DIR}
+
+# Delete PostgreSQL backups older than 10 days
+find $POSTGRES_BACKUP_DIR/* -mtime +10 -exec rm {} \;
+
+# Delete MySQL backups older than 10 days
+find $MYSQL_BACKUP_DIR/* -mtime +10 -exec rm {} \;
